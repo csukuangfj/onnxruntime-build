bd5ab246943f029df9c4f3838ad47d9d40a10f52
Fangjun Kuang <csukuangfj@gmail.com>
2023-10-30 16:47:29 +0800

    small fixes

--- arm-linux-gnueabihf.toolchain.cmake
+++ arm-linux-gnueabihf.toolchain.cmake
@@ -1,6 +1,6 @@
 # Copied from https://github.com/Tencent/ncnn/blob/master/toolchains/arm-linux-gnueabihf.toolchain.cmake
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR armv7)
 
 set(CMAKE_C_COMPILER "arm-none-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "arm-none-linux-gnueabihf-g++")
